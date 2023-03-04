3532f0709a08ad0f91d9693ead03764e0711529d
Leila Arcega <lbarcega@student.apc.edu.ph>
2023-03-04 22:18:33 +0800

    Updated view.php

--- ci4/app/Views/guest/view.php
+++ ci4/app/Views/guest/view.php
@@ -1,3 +1,22 @@
-<body class="profile">
-<h2><?= esc($guest['name']) ?></h2>
-<p><?= esc($guest['note']) ?></p>
+<body class="no-escape">
+  <img class="bg-image" src="images/logo-bg.png" alt="the boy">
+    <div class="container">
+    
+			<div class="small-container">
+				<div class="gallery">
+				<h1><?= esc($guest['name']) ?></h1>
+				<p class="sub"><?= esc($guest['datetime']) ?> <br>
+                <?= esc($guest['email']) ?><br>
+                <?= esc($guest['website']) ?></p>
+				<hr>
+				<p>
+				<?= esc($guest['note']) ?>
+                "I have pledged to sacrifice my <?= esc($guest['organ']) ?> to Him."
+                Signed by,
+                <?= esc($guest['name']) ?>
+				</p>
+        <center>
+        <a href="../"><button type="button" class="yellow" >Go Back.</button></a>
+        </center>
+				</div>
+			</div>
