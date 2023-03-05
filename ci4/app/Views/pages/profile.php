25f5214746101759998372fe06e9cc130673c507
Leila Arcega <lbarcega@student.apc.edu.ph>
2023-03-05 23:38:11 +0800

    Update navbar

--- ci4/app/Views/pages/profile.php
+++ ci4/app/Views/pages/profile.php
@@ -1,11 +1,13 @@
   <body class = "profile">
     <img class="bg-image" src="images/logo-bg.png" alt="the boy">
-    <nav>
-      <a href="<?php echo base_url();?>/profile">Home</a> |
-      <a href="<?php echo base_url();?>/guest">Donors</a> |
-      <a href="<?php echo base_url();?>/guest/create">Make a Donation</a> |
-      <a href="<?php echo base_url();?>/references">References</a> |
-    </nav>
+    <div class="navigation">
+      <nav>
+        <a href="<?php echo base_url();?>/profile">Home</a> |
+        <a href="<?php echo base_url();?>/guest">Donors</a> |
+        <a href="<?php echo base_url();?>/guest/create">Make a Donation</a> |
+        <a href="<?php echo base_url();?>/references">References</a>
+      </nav>
+    </div>
     <div class="small-container">
       <div id = "hands">
         <img src="images/hann1.gif" class="hand1 handy">
