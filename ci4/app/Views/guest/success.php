682c722aa0a76ff9f5c918fdf067c54d38f06df1
Leila Arcega <lbarcega@student.apc.edu.ph>
2023-03-05 23:03:10 +0800

    Updated guest success.php

--- ci4/app/Views/guest/success.php
+++ ci4/app/Views/guest/success.php
@@ -1,2 +1,7 @@
-<body class="no-escape">
-<p>I changed my mind; I don't want your filthy soul. Enjoy the rest of your miserable life.</p>
+<body class="profile">
+    <img class="bg-image" src="images/logo-bg.png" alt="the boy">
+    <div class="modal" style="display:block !important;">
+        <div class="modal-content">
+        <p><span class="redacted">He</span> is looking forward to the coming harvest. Enjoy the rest of your miserable life before then.</p>
+        </div>
+    </div>
